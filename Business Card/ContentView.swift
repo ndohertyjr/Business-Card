6cded7219a5a39d50844e241fdf1bfc838d2ea1d
Neil Doherty <neildohertyjr@gmail.com>
2022-07-28 11:03:49 -0500

    Updated images

--- Business Card/ContentView.swift
+++ Business Card/ContentView.swift
@@ -36,28 +36,39 @@ struct ContentView: View {
                     .font(.system(size: 22))
                 Divider()
                     .frame(width: 1.0, height: 50.0, alignment: .center)
-                    Text("Phone: 123-456-7890")
-                        .frame(width: 200.0, height: 20.0, alignment: .center)
-                        .background(RoundedRectangle(cornerRadius: UIScreen.screenHeight / 5)
-                            .fill(Color(
-                                red: 0.70,
-                                green: 0.75,
-                                blue: 0.76,
-                                opacity: 1.00))
-                            .frame(width: (UIScreen.screenWidth - 25.0), height: 40.0, alignment: .center)
-                            
-                        )
-                        .padding()
-                Text(verbatim: "Email: myemail@gmail.com")
-                    .frame(width: (UIScreen.screenWidth), height: 20.0, alignment: .center)
-                    .background(RoundedRectangle(cornerRadius: UIScreen.screenHeight / 5)
+               
+                    RoundedRectangle(cornerRadius: UIScreen.screenHeight / 5)
                         .fill(Color(
                             red: 0.70,
                             green: 0.75,
                             blue: 0.76,
                             opacity: 1.00))
                         .frame(width: (UIScreen.screenWidth - 25.0), height: 40.0, alignment: .center)
-                    )
+                        .overlay(HStack {
+                            Image(systemName: "phone.fill")
+                                .foregroundColor(.red)
+                                Text("Phone: 123-456-7890")
+                                .frame(width: 300.0, height: 20.0, alignment: .leading)
+                        })
+                .frame(width: UIScreen.screenWidth, height: 50.0, alignment: .center)
+                RoundedRectangle(cornerRadius: UIScreen.screenHeight / 5)
+                    .fill(Color(
+                        red: 0.70,
+                        green: 0.75,
+                        blue: 0.76,
+                        opacity: 1.00))
+                    .frame(width: (UIScreen.screenWidth - 25.0), height: 40.0, alignment: .center)
+                    .overlay(HStack {
+                        Image(systemName: "envelope")
+                            .foregroundColor(.red)
+                        
+                        Text(verbatim: "Email: myemail@gmail.com")
+                            .frame(width: 300.0, height: 20.0, alignment: .leading)
+                    }
+                        
+                
+                  
+                
             }
         }
     }
