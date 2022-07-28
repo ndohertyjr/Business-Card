d1114fb1586f3dd73b5a4af75e10723bd0ccffb4
Neil Doherty <neildohertyjr@gmail.com>
2022-07-28 09:43:10 -0500

    Initial Commit

---
+++ Business Card/Business_CardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Business_CardApp.swift
+//  Business Card
+//
+//  Created by user220431 on 7/28/22.
+//
+
+import SwiftUI
+
+@main
+struct Business_CardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Business CardUITests/Business_CardUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Business_CardUITestsLaunchTests.swift
+//  Business CardUITests
+//
+//  Created by user220431 on 7/28/22.
+//
+
+import XCTest
+
+class Business_CardUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
